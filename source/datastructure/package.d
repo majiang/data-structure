04be456345039c6c38ddf56469b152460f89a52f
majiang <neuron.ohori@gmail.com>
2016-08-17 18:01:22 +0900

    Add KeySortedValue

--- source/datastructure/package.d
+++ source/datastructure/package.d
@@ -26,7 +26,7 @@ struct SortedKeyValue(Key, Value, alias less="a<b")
     Value opIndexOpAssign(string op, V)(in V value, in Key key)
         if (is (typeof (mixin ("Value.init"~op~"V.init")) : Value))
     {
-        return opIndexAssign(opIndex(key) + value, key);
+        return opIndexAssign(mixin ("opIndex(key)"~op~"value"), key);
     }
     /// ditto
     Value opIndex(in Key key) const
@@ -186,13 +186,154 @@ unittest
     t[0] = 1;
     t[1] = 2;
     t[2] = 3;
-    t[3] = 0;
+    t[3] = 4;
+    t[3] %= 4;
+    assert (t[3] == 0);
     assert ([0:1, 1:2, 2:3, 3:0].sortedKeyValue[].equal(t[]));
     assert ([[0, 1], [1, 2], [2, 3], [3, 0]].sortedKeyValue[].equal(t[]));
     alias T = Tuple!(int, int);
     alias P = Tuple!(int, "myKey", int, "myValue");
     assert ([T(0, 1), T(1, 2), T(2, 3), T(3, 0)].sortedKeyValue[].equal(t[]));
     assert ([P(0, 1), P(1, 2), P(2, 3), P(3, 0)].sortedKeyValue[].equal(t[]));
+}
+
+/// Key-value storage with sorted keys.
+KeySortedValue!(Key, Value, lessValue, lessKey) keySortedValue(Key, Value, alias lessValue="a<b", alias lessKey="a<b")()
+{
+    alias Ret = typeof (return);
+    alias ImplKV = typeof (Ret.init.implKV);
+    alias ImplVK = typeof (Ret.init.implVK);
+    return Ret(new ImplVK);
+}
+/// ditto
+struct KeySortedValue(Key, Value, alias lessValue="a<b", alias lessKey="a<b")
+{
+    /// associative array operations.
+    Value opIndexAssign(in Value value, in Key key)
+    {
+        remove(key);
+        implKV[key] = value;
+        implVK.insert(VK(value, key));
+        return value;
+    }
+    /// ditto
+    Value opIndexOpAssign(string op, V)(in V value, in Key key)
+        if (is (typeof (mixin ("Value.init"~op~"V.init")) : Value))
+    {
+        return opIndexAssign(mixin ("opIndex(key)"~op~"value"), key);
+    }
+    /// ditto
+    Value opIndex(in Key key) const
+    {
+        return implKV[key];
+    }
+    /// ditto
+    @property bool empty() const
+    {
+        return length == 0;
+    }
+    /// ditto
+    bool remove(in Key key)
+    {
+        if (auto vp = key in implKV)
+            implVK.removeKey(VK(*vp, key));
+        return implKV.remove(key);
+    }
+    /// ditto
+    @property size_t length() const
+    {
+        return implKV.length;
+    }
+    /// ditto
+    void clear()
+    {
+        implKV.clear;
+        implVK.clear;
+    }
+    /// ditto
+    auto byKey()
+    {
+        return implVK[].map!(_ => _.key);
+    }
+    /// ditto
+    auto byValue()
+    {
+        return implVK[].map!(_ => _.value);
+    }
+    /// ditto
+    auto byKeyValue()
+    {
+        return implVK[].map!(_ => KV(_.key, _.value));
+    }
+    /// ditto
+    alias opIndex = byKeyValue;
+    invariant
+    {
+        assert (implKV.length == implVK.length);
+    }
+private:
+    alias VK = Tuple!(Value, "value", Key, "key");
+    alias KV = Tuple!(Key, "key", Value, "value");
+    RedBlackTree!(VK, ((a, b) => binaryFun!lessValue(a.value, b.value) || (!binaryFun!lessValue(b.value, a.value) && binaryFun!lessKey(a.key, b.key)))) implVK;
+    Value[Key] implKV;
+}
+///
+unittest
+{
+    import std.exception;
+    auto t = keySortedValue!(int, int);
+    t[0].assertThrown!RangeError;
+    t[0] = 0;
+    t[1] = 1;
+    assert (t.length == 2 && t[0] == 0 && t[1] == 1);
+    t[0] += 2;
+    assert (t.length == 2 && t[0] == 2 && t[1] == 1);
+    assert (t.byKey.equal([1, 0]));
+    assert (t.byValue.equal([1, 2]));
+    assert (t[].equal([t.KV(1, 1), t.KV(0, 2)]));
+    t.clear;
+    assert (t.empty);
+}
+/// helper functions for SortedKeyValue.
+auto keySortedValue(alias lessValue="a<b", alias lessKey="a<b", AA)(AA aa)
+    if (isAssociativeArray!AA)
+{
+    static if (is (AA : Value[Key], Key, Value))
+    {
+        auto ret = keySortedValue!(Key, Value, lessValue, lessKey);
+        foreach (kv; aa.byKeyValue)
+            ret[kv.key] = kv.value;
+        return ret;
+    }
+    else static assert (false);
+}
+/// ditto
+auto keySortedValue(alias lessValue="a<b", alias lessKey="a<b", TR)(TR elems)
+    if (isInputRange!TR && is (typeof (elems.front[0])) && is (typeof (elems.front[1])))
+{
+    alias Key = typeof (ElementType!TR.init[0]);
+    alias Value = typeof (ElementType!TR.init[1]);
+    auto ret = keySortedValue!(Key, Value, lessValue, lessKey);
+    foreach (kv; elems)
+        ret[kv[0]] = kv[1];
+    return ret;
+}
+///
+unittest
+{
+    auto t = keySortedValue!(int, int);
+    t[0] = 1;
+    t[1] = 2;
+    t[2] = 3;
+    t[3] = 4;
+    t[3] %= 4;
+    assert (t[3] == 0);
+    assert ([0:1, 1:2, 2:3, 3:0].keySortedValue[].equal(t[]));
+    assert ([[0, 1], [1, 2], [2, 3], [3, 0]].keySortedValue[].equal(t[]));
+    alias T = Tuple!(int, int);
+    alias P = Tuple!(int, "myKey", int, "myValue");
+    assert ([T(0, 1), T(1, 2), T(2, 3), T(3, 0)].keySortedValue[].equal(t[]));
+    assert ([P(0, 1), P(1, 2), P(2, 3), P(3, 0)].keySortedValue[].equal(t[]));
 }
 
 unittest
